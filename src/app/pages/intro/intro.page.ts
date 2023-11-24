97a9f00059e9800c62ab157660fedde9814c9c8e
Asland3 <malthe3@outlook.com>
2023-11-24 16:37:32 +0100

    Update swiper version to 10.3.1

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import Swiper from 'swiper';
+
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
 })
-export class IntroPage implements OnInit {
+export class IntroPage {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined; 
+  swiper?: Swiper;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private navCtrl: NavController) { }
+
+  swiperReady() {
+    // Wait for the component to render completely
+    setTimeout(() => {
+      this.swiper = this.swiperRef?.nativeElement.swiper;
+    }, 1000); 
+  }
+
+
+  next() {
+    this.swiper?.slideNext();
+  }
+
+  start() {
+    this.navCtrl.navigateRoot('/', { animationDirection: 'forward' });
   }
 
 }
