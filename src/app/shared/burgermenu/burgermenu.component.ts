1d08078e27cfadd600d628f9343225f3d8d1ef08
Emma Bang <83577644+emmabang-dat@users.noreply.github.com>
2023-11-26 13:50:52 +0100

    Update background image and searchbar style

---
+++ src/app/shared/burgermenu/burgermenu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-burgermenu',
+  templateUrl: './burgermenu.component.html',
+  styleUrls: ['./burgermenu.component.scss'],
+})
+export class BurgermenuComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
