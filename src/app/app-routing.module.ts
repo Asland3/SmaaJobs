5cc2b333c5e0fe66f54f5d3349b872867c75ba28
Emma Bang <83577644+emmabang-dat@users.noreply.github.com>
2023-11-28 12:02:17 +0100

    Delete unused files and update styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,56 +5,74 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'intro',
-    loadChildren: () => import('./pages/intro/intro.module').then(m => m.IntroPageModule)
+    loadChildren: () =>
+      import('./pages/intro/intro.module').then((m) => m.IntroPageModule),
   },
   {
     path: 'auth',
     children: [
       {
         path: 'login',
-        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+        loadChildren: () =>
+          import('./pages/login/login.module').then((m) => m.LoginPageModule),
       },
       {
         path: 'register',
-        loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
+        loadChildren: () =>
+          import('./pages/register/register.module').then(
+            (m) => m.RegisterPageModule
+          ),
       },
       {
         path: 'forgot-password',
-        loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordPageModule)
-      }
-    ]
+        loadChildren: () =>
+          import('./pages/forgot-password/forgot-password.module').then(
+            (m) => m.ForgotPasswordPageModule
+          ),
+      },
+    ],
   },
   {
     path: 'start',
-    loadChildren: () => import('./pages/start/start.module').then(m => m.StartPageModule)
+    loadChildren: () =>
+      import('./pages/start/start.module').then((m) => m.StartPageModule),
   },
   {
     path: '**',
-    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule) // 404 page
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(
+        (m) => m.NotFoundPageModule
+      ), // 404 page
   },
   {
     path: 'forgot-password-modal',
-    loadChildren: () => import('./modals/forgot-password-modal/forgot-password-modal.module').then( m => m.ForgotPasswordModalPageModule)
+    loadChildren: () =>
+      import(
+        './modals/forgot-password-modal/forgot-password-modal.module'
+      ).then((m) => m.ForgotPasswordModalPageModule),
   },
   {
     path: 'filtermodal',
-    loadChildren: () => import('./pages/filtermodal/filtermodal.module').then( m => m.FiltermodalPageModule)
+    loadChildren: () =>
+      import('./modals/filtermodal/filtermodal.module').then(
+        (m) => m.FiltermodalPageModule
+      ),
   },
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
