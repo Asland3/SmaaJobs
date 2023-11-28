b2063a222ff3d5d6e3e1a14190467126ae660456
Asland3 <malthe3@outlook.com>
2023-11-28 16:06:50 +0100

    sdsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { MenuController, NavController } from '@ionic/angular';
 import { register } from 'swiper/element/bundle';
 
 register();
@@ -12,19 +12,27 @@ register();
 })
 export class AppComponent {
   public pages = [
-    { title: 'Hjem', url: '/home', icon: 'home' },
-    { title: 'Profil', url: '/profile', icon: 'person' },
-    { title: 'Chat', url: '/chat', icon: 'chatbubbles' },
-    { title: 'Fortrolighedspolitik', url: '/privacy-policy', icon: 'document' },
+    { title: 'Hjem', url: '/home', icon: 'home', direction: 'back' },
+    { title: 'Profil', url: '/profile', icon: 'person', direction: 'forward' },
+    { title: 'Chat', url: '/chat', icon: 'chatbubbles', direction: 'forward' },
+    { title: 'Fortrolighedspolitik', url: '/privacy-policy', icon: 'document', direction: 'forward' },
   ];
 
-  constructor(private router: Router, private menu: MenuController) {}
+  constructor(private router: Router, private menu: MenuController, private navCtrl: NavController) {}
 
   logout() {
-    this.router.navigateByUrl('/intro'); 
+    this.navCtrl.navigateBack('/start'); 
     this.menu.close();
   }
   
+  navigateToPage(page: any) {
+    if (page.direction === 'forward') {
+      this.navCtrl.navigateForward(page.url);
+    } else if (page.direction === 'back') {
+      this.navCtrl.navigateBack(page.url);
+    }
+    this.menu.close();
+  }
 
   isActive(url: string): boolean {
     return this.router.url === url;
