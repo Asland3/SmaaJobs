73a35ad8a3e7d406ab3e457bb0d8d3e9c355e915
Asland3 <malthe3@outlook.com>
2023-12-08 15:09:30 +0100

    Fix formatting and add chat functionality

--- src/app/pages/active-chat/active-chat.page.ts
+++ src/app/pages/active-chat/active-chat.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { ChatService } from 'src/app/services/chat-service/chat.service';
 
 @Component({
   selector: 'app-active-chat',
@@ -7,9 +9,31 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./active-chat.page.scss'],
 })
 export class ActiveChatPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  messages: any[] = [];
+  private messagesSubscription!: Subscription;
 
-  ngOnInit() {}
+  constructor(
+    private navCtrl: NavController,
+    private chatService: ChatService
+  ) {}
+
+  ngOnInit() {
+    this.chatService.getMessages().subscribe((data) => {
+      this.messages = data;
+    });
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy() {
+    if (this.messagesSubscription) {
+      this.messagesSubscription.unsubscribe();
+    }
+    console.log('ngOnDestroy');
+  }
+
+  test() {
+    this.chatService.sendMessage('Hello World', '123');
+  }
 
   navigateToChat() {
     this.navCtrl.navigateBack('/chat');
